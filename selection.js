6c20ec83bf682fdce17b17e998062571b8cb6191
vishnu <vvichugit@gmail.com>
2019-12-09 14:59:12 -0600

    added the code for selection sort

---
+++ selection.js
@@ -0,0 +1,22 @@
+//Youtube: https://www.youtube.com/watch?v=g-PGLbMth_g
+function selection(a) {
+  for (let i = 0; i < a.length - 1; i++) {
+    var iMin = i;
+
+    for (let j = i + 1; j < a.length; j++) {
+      if (a[iMin] > a[j]) {
+        iMin = j;
+      }
+    }
+
+    if (iMin != i) {
+      var c = a[iMin];
+      a[iMin] = a[i];
+      a[i] = c;
+    }
+  }
+
+  console.log(a);
+}
+
+selection([2, 7, 1, 3, 9]);
